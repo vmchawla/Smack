7d4968660307f3eb6db85449710b9288a050acd6
Varun Chawla <vmchawla158kb@hotmail.com>
2017-07-26 15:49:10 +0530

    LoginVC Design Complete

---
+++ Controller/LoginVC.swift
@@ -0,0 +1,28 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Varun Chawla on 26/07/17.
+//  Copyright © 2017 Varun Chawla. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        
+        dismiss(animated: true)
+            
+        
+        
+    }
+    
+
+}
